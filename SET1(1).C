c435799cd0289468945afb1203de94c6aa24f1a9
gowthamnvg <nvggowtham@gmail.com>
2017-06-28 14:27:45 +0530

    Update SET1(1).C

--- SET1(1).C
+++ SET1(1).C
@@ -1,1 +1,22 @@
-NFGNFOHGUORHGON
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  clrscr();
+  printf("enter the number");
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("the number is positive");
+  }
+  elseif(a<0)
+  {
+    printf("the number is negative");
+  }
+  else
+  {
+    printf("the number is zero");
+  }
+  getch();
+}
