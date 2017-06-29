878908948d946fa9d1e7fce35d7fe1c6c17e49e3
gowthamnvg <nvggowtham@gmail.com>
2017-06-29 13:47:32 +0530

    Create set1(2).c

---
+++ set1(2).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the num:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("even number");
+}
+else
+{
+printf("odd num");
+}
+getch();
+}
