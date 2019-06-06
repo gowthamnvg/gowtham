cc611fe4b7eb187fb7d48f0436f8a8b9b7acfaf0
gowthamnvg <nvggowtham@gmail.com>
2019-06-06 10:11:18 +0530

    Create number_combination.py

---
+++ number_combination.py
@@ -0,0 +1,10 @@
+inp=int(input())
+S=str(inp)
+b=0
+for i in range(0,len(S)):
+    if int(S[i:i+2])<26 and len(str(int(S[i:i+2])))==2:
+        b=b+1
+if b==2:
+    print(b+b//2)
+else:
+    print(b+b//2+1)
