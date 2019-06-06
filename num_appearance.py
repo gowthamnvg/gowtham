fba1aeca976df41128bdad349f1f132a37fdb386
gowthamnvg <nvggowtham@gmail.com>
2019-06-06 09:38:57 +0530

    Update num_appearance.py

--- num_appearance.py
+++ num_appearance.py
@@ -1,10 +1,10 @@
-number=int(input())
-d=[int(x) for x in input().split()]
-x=[]
-for j in range(0,len(d)):
-    if j==d[j]:
-        x.append(j)
-if len(x)==0:
-    print(-1)
-        
-print(*x)
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+for i in range(0,len(l)):
+    for j in range(i+1,len(l)):
+        if(l[i]==l[j]):
+            l1.append(l[i])
+l2=list(set(l)^set(l1))
+for i in l2:
+    print(i,end=" ")
