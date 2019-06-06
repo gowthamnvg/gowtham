9b989f7b9b5c2b61dc511fbc584f152ddbefed58
gowthamnvg <nvggowtham@gmail.com>
2019-06-06 12:15:36 +0530

    Delete restaurent.py

--- inp1,inp2 = map(int,input().split()) c = list(map(int,input().split())) amount = int(input()) total = (sum(c)-c[inp2])/restaurent.py
+++
@@ -1,8 +0,0 @@
-inp1,inp2 = map(int,input().split())
-c = list(map(int,input().split()))
-amount = int(input())
-total = (sum(c)-c[inp2])//2
-if amount == total:
-  print("Bon Appetit")
-else:
-  print(amount-total)
