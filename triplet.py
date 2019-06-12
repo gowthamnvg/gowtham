4e5dbfd5d4abdb8e61fcb7c28dbdcba3078e4457
gowthamnvg <nvggowtham@gmail.com>
2019-06-12 09:09:26 +0530

    Create triplet.py

---
+++ triplet.py
@@ -0,0 +1,9 @@
+inp1=int(input())
+List=list(map(int,input().split()))
+temp=0
+for i in range(len(List)-2):
+    for j in range(i+1,len(List)-1):
+        for k in range(j+1,len(List)):
+            if List[i]<List[j]<List[k] and i<j<k:
+                temp=temp+1
+print(temp)
