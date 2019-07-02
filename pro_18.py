a95d9bdcaafc354e50b4a88c479673aa9eb2d001
gowthamnvg <nvggowtham@gmail.com>
2019-07-02 18:03:18 +0530

    Update pro_18.py

--- pro_18.py
+++ pro_18.py
@@ -1,16 +1,16 @@
-n,m=map(int,input().split())
-t=[]
-x=0
-for i in range(n):
-    t.append(list(map(int,input().split())))   
-for i in range(n):
-    for j in range(m-1):
-        for k in range(j+1,m+1):
-            if t[i][j:k]==[1]*len(t[i][j:k]):
-                 if all(t[p+i][j:k]==[1]*len(t[p+i][j:k]) for p in range(len(t[i][j:k])-1)):
-                     if len(t[i][j:k])>x:
-                        x=len(t[i][j:k])
-if len(t)==1 and len(t[0])==1 and t[0][0]==1:
+inp1,inp2=map(int,input().split())
+temp=[]
+e=0
+for i in range(inp1):
+    temp.append(list(map(int,input().split())))   
+for i in range(inp1):
+    for s in range(inp2-1):
+        for w in range(s+1,inp2+1):
+            if temp[i][s:w]==[1]*len(temp[i][s:w]):
+                 if all(temp[p+i][s:w]==[1]*len(temp[p+i][s:w]) for p in range(len(temp[i][s:w])-1)):
+                     if len(temp[i][s:w])>e:
+                        e=len(temp[i][s:w])
+if len(temp)==1 and len(temp[0])==1 and temp[0][0]==1:
     print(1)
-for i in range(x):
-    print(*[1]*x) 
+for i in range(e):
+    print(*[1]*e)
