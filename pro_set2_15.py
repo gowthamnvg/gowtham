3b95a95a55f53c3f859d5722850571fb4e404bac
gowthamnvg <nvggowtham@gmail.com>
2019-07-02 17:18:41 +0530

    Create pro_set2_15.py

---
+++ pro_set2_15.py
@@ -0,0 +1,17 @@
+e=int(input())
+b=list(map(int,input().split()))
+n=s=[]
+for i in range(0,e):
+    n=list(bin(b[i]))
+    n=n[2:]
+    s.append(n)
+s=sorted(s)
+s=s[::-1]
+for i in range(0,e):
+    y=1
+    z=0
+    for j in range(len(s[i])-1,-1,-1):
+        z=z+(int(s[i][j])*y)
+        y=y*2
+    print(z)
+ 
